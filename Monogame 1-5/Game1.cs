d93964f9a51a230e2cb9d68c0d5211c971de1821
Naika Jith <naika.jith@outlook.com>
2026-06-15 07:09:59 -0400

    Working on it!

--- Monogame 1-5/Game1.cs
+++ Monogame 1-5/Game1.cs
@@ -18,10 +18,6 @@ namespace Monogame_1_5
 
         Screen currentScreen;
 
-
-
-
-
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
@@ -70,9 +66,31 @@ namespace Monogame_1_5
         Texture2D proceedTexture;
         Rectangle proceedRect;
 
+
+        // Dialogue boxes
+        Texture2D dialogue1Texture;
+        Texture2D dialogue2Texture;
+        Texture2D dialogue3Texture;
+        Texture2D dialogue4Texture;
+        Texture2D dialogue5Texture;
+        Texture2D dialogue6Texture;
+        Texture2D dialogue7Texture;
+        Texture2D dialogue8Texture;
+
+        Rectangle dialogue1Rect;
+        Rectangle dialogue2Rect;
+        Rectangle dialogue3Rect;
+        Rectangle dialogue4Rect;
+        Rectangle dialogue5Rect;
+        Rectangle dialogue6Rect;
+        Rectangle dialogue7Rect;
+        Rectangle dialogue8Rect;
+
+
         MouseState mouseState;
         MouseState previousMouse;
 
+        float timer;
 
         public Game1()
         {
@@ -92,6 +110,8 @@ namespace Monogame_1_5
 
             currentScreen = Screen.Intro;
 
+            timer = 0;
+
             //Screens
 
             introRect = new Rectangle(0, 0, 950, 600);
@@ -102,15 +122,29 @@ namespace Monogame_1_5
 
             //Characters
 
-            malfoyRect = new Rectangle(30, 50, 150, 90);
-            damonRect = new Rectangle(30, 50, 150, 90);
-            snapeRect = new Rectangle(30, 50, 150, 90);
-            romeoRect = new Rectangle(30, 50, 150, 90);
-            axelRect = new Rectangle(30, 50, 150, 80);
+            malfoyRect = new Rectangle(5, 320, 310, 290);
+            damonRect = new Rectangle(200, 317, 187, 290);
+            snapeRect = new Rectangle(340, 320, 300, 290);
+            axelRect = new Rectangle(530, 330, 250, 290);  
+            romeoRect = new Rectangle(720, 330, 250, 270);
+
 
             //Buttons
 
             proceedRect = new Rectangle(23, 14, 325, 115);
+
+            //Dialogues
+
+            timer = 0;
+
+            dialogue1Rect = new Rectangle(150, 50, 150, 150);
+            dialogue2Rect = new Rectangle(150, 50, 150, 150);
+            dialogue3Rect = new Rectangle(150, 50, 150, 150);
+            dialogue4Rect = new Rectangle(150, 50, 150, 150);
+            dialogue5Rect = new Rectangle(150, 50, 150, 150);
+            dialogue6Rect = new Rectangle(150, 50, 150, 150);
+            dialogue7Rect = new Rectangle(150, 50, 150, 150);
+            dialogue8Rect = new Rectangle(150, 50, 150, 150);
 
 
 
@@ -145,6 +179,17 @@ namespace Monogame_1_5
 
             font = Content.Load<SpriteFont>("font");
 
+            //Dialogues
+
+            dialogue1Texture = Content.Load<Texture2D>("dialogue1");
+            dialogue2Texture = Content.Load<Texture2D>("dialogue2");
+            dialogue3Texture = Content.Load<Texture2D>("dialogue3");
+            dialogue4Texture = Content.Load<Texture2D>("dialogue4");
+            dialogue5Texture = Content.Load<Texture2D>("dialogue5");
+            dialogue6Texture = Content.Load<Texture2D>("dialogue6");
+            dialogue7Texture = Content.Load<Texture2D>("dialogue7");
+            dialogue8Texture = Content.Load<Texture2D>("dialogue8");
+
 
             // TODO: use this.Content to load your game content here
         }
@@ -159,65 +204,54 @@ namespace Monogame_1_5
             previousMouse = mouseState;
             mouseState = Mouse.GetState();
 
+            bool clicked = mouseState.LeftButton == ButtonState.Pressed && previousMouse.LeftButton == ButtonState.Released;
+
             this.Window.Title = mouseState.Position.ToString();
 
 
             if (currentScreen == Screen.Intro)
             {
-                if (mouseState.LeftButton == ButtonState.Pressed && previousMouse.LeftButton == ButtonState.Released)
-                {
+                timer += (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-                    if (proceedRect.Contains(mouseState.Position))
+                if (timer >= 5f)
+                {
+                    if (clicked && proceedRect.Contains(mouseState.Position))
                     {
                         currentScreen = Screen.Detention;
+                        timer = 0;
                     }
-
                 }
-
             }
 
 
             else if (currentScreen == Screen.Detention)
             {
-                if (mouseState.LeftButton == ButtonState.Pressed && previousMouse.LeftButton == ButtonState.Released)
+                timer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+                if (timer >= 24 && clicked && proceedRect.Contains(mouseState.Position))
                 {
-
-                    if (proceedRect.Contains(mouseState.Position))
-                    {
-                        currentScreen = Screen.Hallway;
-                    }
-
+                    currentScreen = Screen.Hallway;
+                    timer = 0;
                 }
-
             }
+
+
 
 
             else if (currentScreen == Screen.Hallway)
             {
-                if (mouseState.LeftButton == ButtonState.Pressed && previousMouse.LeftButton == ButtonState.Released)
+                if (clicked && proceedRect.Contains(mouseState.Position))
                 {
-
-                    if (proceedRect.Contains(mouseState.Position))
-                    {
-                        currentScreen = Screen.Abandoned;
-                    }
-
+                    currentScreen = Screen.Abandoned;
                 }
-
             }
 
             else if (currentScreen == Screen.Abandoned)
             {
-                if (mouseState.LeftButton == ButtonState.Pressed && previousMouse.LeftButton == ButtonState.Released)
+                if (clicked && proceedRect.Contains(mouseState.Position))
                 {
-
-                    if (proceedRect.Contains(mouseState.Position))
-                    {
-                        currentScreen = Screen.End;
-                    }
-
+                    currentScreen = Screen.End;
                 }
-
             }
 
             base.Update(gameTime);
@@ -238,24 +272,97 @@ namespace Monogame_1_5
                 _spriteBatch.Draw(proceedTexture, proceedRect, Color.White);
                 _spriteBatch.DrawString(font, "Lorem Ipsum Sit Dolor", new Vector2(20, 500), Color.Gold);
 
+                if (proceedRect.Contains(mouseState.Position))
+                {
+                    currentScreen = Screen.Detention;
+                    timer = 0;
+                }
+
             }
 
             else if (currentScreen == Screen.Detention)
             {
+
+                if (timer >= 24)
+                {
+                    _spriteBatch.Draw(proceedTexture, proceedRect, Color.White);
+                }
+
                 _spriteBatch.Draw(detentionTexture, detentionRect, Color.White);
-                _spriteBatch.Draw(proceedTexture, proceedRect, Color.White);
+                _spriteBatch.Draw(malfoyTexture, malfoyRect, Color.White);
+                _spriteBatch.Draw(damonTexture, damonRect, Color.White);
+                _spriteBatch.Draw(snapeTexture, snapeRect, Color.White);
+                _spriteBatch.Draw(axelTexture, axelRect, Color.White);
+                _spriteBatch.Draw(romeoTexture, romeoRect, Color.White);
+
+                _spriteBatch.Draw(detentionTexture, detentionRect, Color.White);
+
+                _spriteBatch.Draw(malfoyTexture, malfoyRect, Color.White);
+                _spriteBatch.Draw(damonTexture, damonRect, Color.White);
+                _spriteBatch.Draw(snapeTexture, snapeRect, Color.White);
+                _spriteBatch.Draw(axelTexture, axelRect, Color.White);
+                _spriteBatch.Draw(romeoTexture, romeoRect, Color.White);
+
+                if (timer < 3)
+                {
+                    _spriteBatch.Draw(dialogue1Texture, dialogue1Rect, Color.White);
+                }
+                else if (timer < 6)
+                {
+                    _spriteBatch.Draw(dialogue2Texture, dialogue2Rect, Color.White);
+                }
+                else if (timer < 9)
+                {
+                    _spriteBatch.Draw(dialogue3Texture, dialogue3Rect, Color.White);
+                }
+                else if (timer < 12)
+                {
+                    _spriteBatch.Draw(dialogue4Texture, dialogue4Rect, Color.White);
+                }
+                else if (timer < 15)
+                {
+                    _spriteBatch.Draw(dialogue5Texture, dialogue5Rect, Color.White);
+                }
+                else if (timer < 18)
+                {
+                    _spriteBatch.Draw(dialogue6Texture, dialogue6Rect, Color.White);
+                }
+                else if (timer < 21)
+                {
+                    _spriteBatch.Draw(dialogue7Texture, dialogue7Rect, Color.White);
+                }
+                else if (timer < 24)
+                {
+                    _spriteBatch.Draw(dialogue8Texture, dialogue8Rect, Color.White);
+                }
+                else
+                {
+                    _spriteBatch.Draw(proceedTexture, proceedRect, Color.White);
+                }
+
+
             }
 
             else if (currentScreen == Screen.Hallway)
             {
                 _spriteBatch.Draw(hallwayTexture, hallwayRect, Color.White);
                 _spriteBatch.Draw(proceedTexture, proceedRect, Color.White);
+                _spriteBatch.Draw(malfoyTexture, malfoyRect, Color.White);
+                _spriteBatch.Draw(damonTexture, damonRect, Color.White);
+                _spriteBatch.Draw(snapeTexture, snapeRect, Color.White);
+                _spriteBatch.Draw(axelTexture, axelRect, Color.White);
+                _spriteBatch.Draw(romeoTexture, romeoRect, Color.White);
             }
 
             else if (currentScreen == Screen.Abandoned)
             {
                 _spriteBatch.Draw(abandonedTexture, abandonedRect, Color.White);
                 _spriteBatch.Draw(proceedTexture, proceedRect, Color.White);
+                _spriteBatch.Draw(malfoyTexture, malfoyRect, Color.White);
+                _spriteBatch.Draw(damonTexture, damonRect, Color.White);
+                _spriteBatch.Draw(snapeTexture, snapeRect, Color.White);
+                _spriteBatch.Draw(axelTexture, axelRect, Color.White);
+                _spriteBatch.Draw(romeoTexture, romeoRect, Color.White);
             }
 
             else if (currentScreen == Screen.End)
